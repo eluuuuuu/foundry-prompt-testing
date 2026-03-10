90431c5771e290483cab693ef0de5c9a79f59cba
eluuuuuu <123312419+eluuuuuu@users.noreply.github.com>
2026-03-10 12:47:16 +0000

    Update Translator_Test.py

--- Translator_Test.py
+++ Translator_Test.py
@@ -1,19 +1,18 @@
-from azure.identity import DefaultAzureCredential
 from azure.ai.projects import AIProjectClient
+from azure.core.credentials import AzureKeyCredential
+import os
 
-# Connect to Foundry
 myEndpoint = "https://eliasabdo718-1008-resource.services.ai.azure.com/api/projects/eliasabdo718-1008"
 
 project_client = AIProjectClient(
     endpoint=myEndpoint,
-    credential=DefaultAzureCredential(),
+    credential=AzureKeyCredential(os.environ["AZURE_API_KEY"]),
 )
 
 myAgent = "Elias-language-Translator"
 myVersion = "3"
 openai_client = project_client.get_openai_client()
 
-# Reusable function
 def ask_agent(prompt):
     response = openai_client.responses.create(
         model="gpt-4.1",
@@ -22,15 +21,10 @@ def ask_agent(prompt):
     )
     return response.output_text
 
-# The sentence we are testing
 sentence = "Good morning, I hope you have a wonderful day"
-
-# List of languages to translate into
 languages = ["French", "Spanish", "German", "Italian", "Arabic", "Japanese", "Portuguese"]
 
-# Loop through each language and print the translation
 for language in languages:
     print(f"=== {language} ===")
     print(ask_agent(f"Translate this to {language}: '{sentence}'"))
     print()
-
