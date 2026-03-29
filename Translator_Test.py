412822ae4053704d796fce32f632b10c9aadeb77
elias abdo <eliasabdoo@yahoo.co.uk>
2026-03-29 18:27:41 +0100

    fix api version

--- Translator_Test.py
+++ Translator_Test.py
@@ -7,7 +7,7 @@ load_dotenv()
 client = AzureOpenAI(
     api_key=os.environ["AZURE_API_KEY"],
     azure_endpoint=os.environ["AZURE_ENDPOINT"],
-    api_version="2025-03-01-preview",
+    api_version="2024-02-01",
 )
 
 def translate(sentence: str, language: str) -> str:
